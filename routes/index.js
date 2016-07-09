014cb1a0b5bd9cae2441378597cb113bf33b0edd
brandonsack <bsack@sas.upenn.edu>
2016-07-09 15:58:36 -0400

    seeded database and finished routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var Exercise = require('../models/Exercise');
+var _ = require('underscore')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('selector', {user: req.user});
+});
+
+router.post('/', function(req, res, next) {
+	Exercise.find(function(err, exercises) {
+		if (err) return next(err)
+		var arr = [];
+		for (var i = 0; i < req.body.muscles.length; i++) {
+			arr.push([]);
+		}
+		for (var i = 0; i < exercises.length; i++) {
+			for (var j = 0; j < req.body.muscles.length; j++) {
+				for (var k = 0; k < exercises[i].muscles.length; k++) {
+					if (exercises[i].muscles[k] === req.body.muscles[j] && (req.body.equipment.indexOf(exercises[i].equipment) >= 0 || req.body.equipment[req.body.equipment.length - 1])) {
+						arr[j].push(excercises[i]);
+					}
+				}
+				
+			}
+		}
+		var sets = 12 / req.body.muscles.length;
+		var workout = [];
+		for (var i = 0; i < arr.length; i++) {
+			var temp = _.shuffle(arr[i]);
+			for (var j = 0; j < sets; j++) {
+				workout.push(temp.pop());
+			}
+		}
+		res.render('workout', {workout: workout});
+	})
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = function(passport) {
+	router.get('/register', function(req, res, next) {
+		res.render('register')
+	})
 
-module.exports = router;
+	router.post('/register', function(req, res, next) {
+		var error = {}
+		if (!req.body.username) {
+			error.username = "Bad username"
+		}
+		if (!req.body.password) {
+			error.password = "Bad password"
+		}
+		if (req.body.password !== req.body.passwordRepeat) {
+			error.passwordr = "Passwords don't match"
+		}
+		for (var key in error) {
+	        if (error.hasOwnProperty(key)) {
+	            res.render('register', {error: error});
+	            return;
+	        }
+	    }
+	    var u = new User({
+	    	username: req.body.username,
+	    	password: req.body.password,
+	    })
+	    u.save(function(err) {
+	    	if (err) {
+	    		console.log(err)
+	    		res.status(500).redirect('/register')
+	    		return;
+	    	}
+	    	res.render('login', {user: u})
+	    })
+	})
+
+	router.get('/login', function(req, res, next) {
+		res.render('login');
+	});
+
+	router.post('/login', passport.authenticate('local', { 
+		successRedirect: '/initializeCart',
+	    failureRedirect: '/login'})
+	);
+
+	router.use('/login/facebook', passport.authenticate('facebook')
+	);
+
+	router.use('/auth/facebook/callback', passport.authenticate('facebook', { 
+		successRedirect: '/',
+	    failureRedirect: '/login'})
+	);
+
+	router.get('/logout', function(req, res, next) {
+		req.logout()
+		res.redirect('/login')
+	})
+	return router;
+}
